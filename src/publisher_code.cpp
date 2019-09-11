3892a6ace3698caad71f44c76b76d5051d0ab56b
nicolemariegraf <nmg63@case.edu>
2019-09-11 16:00:46 -0700

    Update publisher package

--- src/publisher_code.cpp
+++ src/publisher_code.cpp
@@ -1,11 +1,12 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 #include <sstream>
+#include "publisher_package/topic_name.h"
 int main(int argc, char **argv)
 {
 ros::init(argc, argv, "talker");
 ros::NodeHandle n;
-ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
+ros::Publisher chatter_pub = n.advertise<std_msgs::String>("topic_name", 1000);
 ros::Rate loop_rate(1);
 int count = 0;
 while (ros::ok())
